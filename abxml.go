156f1bc32bcafb68e0eec04197094dcac91b02f4
lberrymage <logan@lberrymage.dev>
2022-04-03 00:52:36 -0800

    Remove unused parseXMLElement function
    This was a vestige of the initial parsing system.

--- abxml.go
+++ abxml.go
@@ -1,12 +1,5 @@
 package apkstat
 
-import (
-	"encoding/binary"
-	"encoding/xml"
-	"fmt"
-	"io"
-)
-
 type resXMLTreeHeader struct {
 	Header resChunkHeader
 }
@@ -43,55 +36,4 @@ type resXMLTreeAttribute struct {
 	Name       resStringPoolRef
 	RawValue   resStringPoolRef
 	TypedValue resValue
-}
-
-func parseXMLElement(sr *io.SectionReader, sp map[resStringPoolRef]string) (*xml.StartElement, error) {
-	var e xml.StartElement
-
-	node := new(resXMLTreeNode)
-	if err := binary.Read(sr, binary.LittleEndian, node); err != nil {
-		return nil, err
-	}
-
-	element := new(resXMLTreeAttrExt)
-	if err := binary.Read(sr, binary.LittleEndian, element); err != nil {
-		return nil, err
-	}
-	e.Name = xml.Name{Space: sp[element.NS], Local: sp[element.Name]}
-
-	for i := 0; i < int(element.AttributeCount); i++ {
-		attr := new(resXMLTreeAttribute)
-		if err := binary.Read(sr, binary.LittleEndian, attr); err != nil {
-			return nil, err
-		}
-
-		var value string
-		switch attr.TypedValue.DataType {
-		case typeNull:
-			value = ""
-		case typeReference:
-			value = fmt.Sprintf("@0x%08X", attr.TypedValue.Data)
-		case typeString:
-			value = sp[attr.RawValue]
-		case typeFloat:
-			value = fmt.Sprintf("%f", float32(attr.TypedValue.Data))
-		case typeIntDec:
-			value = fmt.Sprintf("%d", attr.TypedValue.Data)
-		case typeIntHex:
-			value = fmt.Sprintf("0x%08X", attr.TypedValue.Data)
-		case typeIntBoolean:
-			if attr.TypedValue.Data == 1 {
-				value = "true"
-			} else {
-				value = "false"
-			}
-		}
-
-		e.Attr = append(e.Attr, xml.Attr{
-			Name:  xml.Name{Space: sp[attr.NS], Local: sp[attr.Name]},
-			Value: value,
-		})
-	}
-
-	return &e, nil
 }
