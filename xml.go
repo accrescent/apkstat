9ef555f8a7eeb803b8352a146f16d4cb841f3525
lberrymage <logan@lberrymage.dev>
2022-05-25 01:32:46 -0800

    Consolidate abxml.go into xml.go
    This change really should have been a part of 92ee343: Reorganize and
    consolidate file structure, but apparently I missed it.

--- xml.go
+++ xml.go
@@ -209,3 +209,41 @@ func (f *XMLFile) nsPrefix(ns resStringPoolRef, name resStringPoolRef) string {
 		return fmt.Sprintf("%s:%s", f.stringPool[f.namespaces[ns]], f.stringPool[name])
 	}
 }
+
+type resXMLTreeHeader struct {
+	Header resChunkHeader
+}
+
+type resXMLTreeNode struct {
+	Header     resChunkHeader
+	LineNumber uint32
+	Comment    resStringPoolRef
+}
+
+type resXMLTreeNamespaceExt struct {
+	Prefix resStringPoolRef
+	URI    resStringPoolRef
+}
+
+type resXMLTreeEndElementExt struct {
+	NS   resStringPoolRef
+	Name resStringPoolRef
+}
+
+type resXMLTreeAttrExt struct {
+	NS             resStringPoolRef
+	Name           resStringPoolRef
+	AttributeStart uint16
+	AttributeSize  uint16
+	AttributeCount uint16
+	IDIndex        uint16
+	ClassIndex     uint16
+	StyleIndex     uint16
+}
+
+type resXMLTreeAttribute struct {
+	NS         resStringPoolRef
+	Name       resStringPoolRef
+	RawValue   resStringPoolRef
+	TypedValue resValue
+}
