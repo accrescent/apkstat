41345ad41d8201203f1239978bdbe40a04de506e
lberrymage <logan@lberrymage.dev>
2022-06-20 23:15:37 -0800

    Add initial Activities field to Application

--- manifest.go
+++ manifest.go
@@ -10,20 +10,55 @@ type Manifest struct {
 }
 
 type Application struct {
-	AllowTaskReparenting         bool   `xml:"http://schemas.android.com/apk/res/android allowTaskReparenting,attr"`
-	AllowBackup                  bool   `xml:"http://schemas.android.com/apk/res/android allowBackup,attr"`
-	BackupAgent                  string `xml:"http://schemas.android.com/apk/res/android backupAgent,attr"`
-	BackupInForeground           bool   `xml:"http://schemas.android.com/apk/res/android backupInForeground,attr"`
-	DataExtractionRules          string `xml:"http://schemas.android.com/apk/res/android dataExtractionRules,attr"`
-	Debuggable                   bool   `xml:"http://schemas.android.com/apk/res/android debuggable,attr"`
-	Label                        string `xml:"http://schemas.android.com/apk/res/android label,attr"`
-	ManageSpaceActivity          string `xml:"http://schemas.android.com/apk/res/android manageSpaceActivity,attr"`
-	Name                         string `xml:"http://schemas.android.com/apk/res/android name,attr"`
-	NetworkSecurityConfig        string `xml:"http://schemas.android.com/apk/res/android networkSecurityConfig,attr"`
-	RequestLegacyExternalStorage bool   `xml:"http://schemas.android.com/apk/res/android requestLegacyExternalStorage,attr"`
-	SupportsRTL                  bool   `xml:"http://schemas.android.com/apk/res/android supportsRtl,attr"`
-	TestOnly                     bool   `xml:"http://schemas.android.com/apk/res/android testOnly,attr"`
-	UsesCleartextTraffic         bool   `xml:"http://schemas.android.com/apk/res/android usesCleartextTraffic,attr"`
+	AllowTaskReparenting         bool       `xml:"http://schemas.android.com/apk/res/android allowTaskReparenting,attr"`
+	AllowBackup                  bool       `xml:"http://schemas.android.com/apk/res/android allowBackup,attr"`
+	BackupAgent                  string     `xml:"http://schemas.android.com/apk/res/android backupAgent,attr"`
+	BackupInForeground           bool       `xml:"http://schemas.android.com/apk/res/android backupInForeground,attr"`
+	DataExtractionRules          string     `xml:"http://schemas.android.com/apk/res/android dataExtractionRules,attr"`
+	Debuggable                   bool       `xml:"http://schemas.android.com/apk/res/android debuggable,attr"`
+	Label                        string     `xml:"http://schemas.android.com/apk/res/android label,attr"`
+	ManageSpaceActivity          string     `xml:"http://schemas.android.com/apk/res/android manageSpaceActivity,attr"`
+	Name                         string     `xml:"http://schemas.android.com/apk/res/android name,attr"`
+	NetworkSecurityConfig        string     `xml:"http://schemas.android.com/apk/res/android networkSecurityConfig,attr"`
+	RequestLegacyExternalStorage bool       `xml:"http://schemas.android.com/apk/res/android requestLegacyExternalStorage,attr"`
+	SupportsRTL                  bool       `xml:"http://schemas.android.com/apk/res/android supportsRtl,attr"`
+	TestOnly                     bool       `xml:"http://schemas.android.com/apk/res/android testOnly,attr"`
+	UsesCleartextTraffic         bool       `xml:"http://schemas.android.com/apk/res/android usesCleartextTraffic,attr"`
+	Activities                   []Activity `xml:"activity"`
+}
+
+type Activity struct {
+	Exported      string         `xml:"http://schemas.android.com/apk/res/android exported,attr"`
+	Label         string         `xml:"http://schemas.android.com/apk/res/android label,attr"`
+	Name          string         `xml:"http://schemas.android.com/apk/res/android name,attr"`
+	IntentFilters []IntentFilter `xml:"intent-filter"`
+}
+
+type IntentFilter struct {
+	Priority   int        `xml:"http://schemas.android.com/apk/res/android priority,attr"`
+	Order      int        `xml:"http://schemas.android.com/apk/res/android order,attr"`
+	AutoVerify bool       `xml:"http://schemas.android.com/apk/res/android autoVerify,attr"`
+	Actions    []Action   `xml:"action"`
+	Categories []Category `xml:"category"`
+	Data       []Data     `xml:"data"`
+}
+
+type Action struct {
+	Name string `xml:"http://schemas.android.com/apk/res/android name,attr"`
+}
+
+type Category struct {
+	Name string `xml:"http://schemas.android.com/apk/res/android name,attr"`
+}
+
+type Data struct {
+	Scheme      string `xml:"http://schemas.android.com/apk/res/android scheme,attr"`
+	Host        string `xml:"http://schemas.android.com/apk/res/android host,attr"`
+	Port        string `xml:"http://schemas.android.com/apk/res/android port,attr"`
+	Path        string `xml:"http://schemas.android.com/apk/res/android path,attr"`
+	PathPattern string `xml:"http://schemas.android.com/apk/res/android pathPattern,attr"`
+	PathPrefix  string `xml:"http://schemas.android.com/apk/res/android pathPrefix,attr"`
+	MimeType    string `xml:"http://schemas.android.com/apk/res/android mimeType,attr"`
 }
 
 type UsesPermission struct {
